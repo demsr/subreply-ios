474cfef0233fdb288686ea9d5c3be8dca87a4349
Dennis Sumser <dennis.sumser@schmolck.de>
2025-07-23 19:23:24 +0200

    Initial Commit

---
+++ subreply/subreplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  subreplyApp.swift
+//  subreply
+//
+//  Created by Dennis Sumser on 23.07.25.
+//
+
+import SwiftUI
+
+@main
+struct subreplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
